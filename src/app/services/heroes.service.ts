359134e58f930fe3739e40eadf43c27c57eccf67
AlfredoMartinezMartinez <alfredo.martinez1@alu.uclm.es>
2018-03-10 03:10:05 +0100

    put y post en firebase

--- src/app/services/heroes.service.ts
+++ src/app/services/heroes.service.ts
@@ -1,8 +1,42 @@
 import { Injectable } from '@angular/core';
+import {Http,Headers} from '@angular/http';
+import {Heroe} from '../interfaces/heroe.interface';
+import 'rxjs/Rx';
 
 @Injectable()
 export class HeroesService {
 
-  constructor() { }
+  heroesUrl:string ="https://heroresapp-56636.firebaseio.com/heroes.json"
+  heroeUrl:string ="https://heroresapp-56636.firebaseio.com/heroes/"
+
+  constructor(private http:Http) { }
+
+  nuevoHeroe(heroe:Heroe){
+    let body=JSON.stringify(heroe);
+    let headers = new Headers({
+      'Content-Type':'application/json'
+    });
+
+    return this.http.post(this.heroesUrl,body,{headers}).map(res=>{
+      console.log(res.json());
+      return res.json();
+    })
+  }
+
+  actualizarHeroe(heroe:Heroe,key$:string){
+    console.log(key$);
+    let body=JSON.stringify(heroe);
+    let headers = new Headers({
+      'Content-Type':'application/json'
+    });
+
+    let url = `${ this.heroeUrl }/$ { key$ }.json`;
+    return this.http.put(url,body,{headers}).map(res=>{
+      console.log(res.json());
+      return res.json();
+    })
+  }
+
+
 
 }
